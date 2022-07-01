f0c1c3884ee81602100e04a826afd96c5648bb6d
Roman Gulevich <R.I.Gulevich@gmail.com>
2022-07-01 15:36:18 +0300

    "Module4 - Final Taskf after switch"

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        System.out.println(divider == 0 ? "division by zero" :
+                dividend / divider * divider == dividend ? "can be divided completely" :
+                        "cannot be divided completely");
     }
 }
